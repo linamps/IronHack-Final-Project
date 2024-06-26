59c437bc8e8f3845221f261321a60a83e974d628
linamps <linamps@gmail.com>
2024-06-26 16:13:36 +0200

    Update model and readme

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 ## Git Repository
 https://github.com/linamps/IronHack-Final-Project.git
+
+## Tableau Dashboard
+https://public.tableau.com/views/AirbnbPriceAnalysis_17193244434260/3_ModelSelection?:language=en-US&:sid=&:display_count=n&:origin=viz_share_link
+
--- model_selection_training.py
+++ model_selection_training.py
@@ -5,19 +5,35 @@ from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, ExtraTreesRegressor
 from sklearn.svm import SVR
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
 import numpy as np
-
+import joblib
+import os
 
 # Load processed and cleaned data
-df = pd.read_csv('airbnb_merged_df.csv')
+df = pd.read_csv('merged_df_before_encoding.csv')
 
-# Split training and test sets
-X = df.drop(['Price','Attraction Index','Restraunt Index','Cleanliness Rating'], axis = 1)
+# Define feature columns and target
+X = df.drop(['Price','Attraction Index','Restraunt Index','Cleanliness Rating', 'Normalised Attraction Index', 'Normalised Restraunt Index','Unnamed: 0'], axis = 1)
 y = df['Price']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 42)
+# Split training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
 
-# Models to be trained.
+# Define categorical and numerical columns
+categorical_features = ['City', 'Day', 'Room Type', 'visits', 'Population']
+numerical_features = [col for col in X.columns if col not in categorical_features]
+
+# Define preprocessor
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', StandardScaler(), numerical_features),
+        ('cat', OneHotEncoder(handle_unknown='ignore'), categorical_features)
+    ])
+
+# Models to be trained
 models = {
     'Linear Regression': LinearRegression(),
     'Decision Tree': DecisionTreeRegressor(random_state=42),
@@ -27,20 +43,22 @@ models = {
     'Extra Trees Regressor': ExtraTreesRegressor(random_state=42)
 }
 
-
 # Model selection
 results = {}
 for name, model in models.items():
+    # Create a pipeline with the preprocessor and model
+    pipeline = Pipeline(steps=[('preprocessor', preprocessor), ('model', model)])
+    
     # Perform cross-validation for MSE
-    mse_cv_scores = cross_val_score(model, X_train, y_train, cv=5, scoring='neg_mean_squared_error')
+    mse_cv_scores = cross_val_score(pipeline, X_train, y_train, cv=5, scoring='neg_mean_squared_error')
     mse_scores = -mse_cv_scores
 
     # Perform cross-validation for MAE
-    mae_cv_scores = cross_val_score(model, X_train, y_train, cv=5, scoring='neg_mean_absolute_error')
+    mae_cv_scores = cross_val_score(pipeline, X_train, y_train, cv=5, scoring='neg_mean_absolute_error')
     mae_scores = -mae_cv_scores
 
     # Perform cross-validation for R²
-    r2_cv_scores = cross_val_score(model, X_train, y_train, cv=5, scoring='r2')
+    r2_cv_scores = cross_val_score(pipeline, X_train, y_train, cv=5, scoring='r2')
 
     results[name] = {
         'Mean MSE': np.mean(mse_scores),
@@ -59,13 +77,14 @@ best_model_name = min(results, key=lambda x: results[x]['Mean MSE'])
 best_model = models[best_model_name]
 print(f"Best Model: {best_model_name}")
 
-# Train and Evaluate with Extra Trees Regressor
+# Create a pipeline with the best model
+best_pipeline = Pipeline(steps=[('preprocessor', preprocessor), ('model', best_model)])
 
 # Train the best model
-best_model.fit(X_train, y_train)
+best_pipeline.fit(X_train, y_train)
 
 # Predict on the test set
-y_pred = best_model.predict(X_test)
+y_pred = best_pipeline.predict(X_test)
 
 # Evaluate the model
 mse = mean_squared_error(y_test, y_pred)
@@ -81,6 +100,9 @@ summary_df = pd.DataFrame(results).T
 models_summary = pd.DataFrame(summary_df)
 print(models_summary)
 
+
 # Save model
 import joblib
-joblib.dump(best_model, 'airbnb_price_predictor.pkl')
+
+# Save the model to a file
+joblib.dump(best_model, 'airbnb_price_predictor2.pkl')
